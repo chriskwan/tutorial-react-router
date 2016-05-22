fc12773541d79cdee2699b82a44aae811057e7bc
Chris Kwan <chriswkwan@gmail.com>
2016-05-22 00:05:28 -0400

    Add code for 13-server-rendering

--- lessons/01-setting-up/index.js
+++ lessons/01-setting-up/index.js
@@ -1,23 +1,11 @@
+// index.js
 import React from 'react'
 import { render } from 'react-dom'
-import App from './modules/App'
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
-import Home from './modules/Home'
-import About from './modules/About'
-import Repos from './modules/Repos'
-import Repo from './modules/Repo'
+import { Router, browserHistory } from 'react-router'
+// import routes and pass them into <Router/>
+import routes from './modules/routes'
 
-//render(<App/>, document.getElementById('app'))
-render((
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-
-      <IndexRoute component={Home}/>
-
-      <Route path="/repos" component={Repos}>
-        <Route path="/repos/:userName/:repoName" component={Repo}/>
-      </Route>
-      <Route path="/about" component={About}/>
-    </Route>
-  </Router>
-), document.getElementById('app'))
+render(
+  <Router routes={routes} history={browserHistory}/>,
+  document.getElementById('app')
+)
