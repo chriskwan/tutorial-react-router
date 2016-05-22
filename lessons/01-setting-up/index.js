959b773d867cc896062534b908c15fd0358af188
Chris Kwan <chriswkwan@gmail.com>
2016-05-21 20:27:41 -0400

    Add code for 06-params

--- lessons/01-setting-up/index.js
+++ lessons/01-setting-up/index.js
@@ -4,12 +4,14 @@ import App from './modules/App'
 import { Router, Route, hashHistory } from 'react-router'
 import About from './modules/About'
 import Repos from './modules/Repos'
+import Repo from './modules/Repo'
 
 //render(<App/>, document.getElementById('app'))
 render((
   <Router history={hashHistory}>
     <Route path="/" component={App}>
       <Route path="/repos" component={Repos}/>
+      <Route path="/repos/:userName/:repoName" component={Repo}/>
       <Route path="/about" component={About}/>
     </Route>
   </Router>
