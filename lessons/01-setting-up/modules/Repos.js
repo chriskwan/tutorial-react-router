277adf3a02594ae5884cff1efdfddea240522a57
Chris Kwan <chriswkwan@gmail.com>
2016-05-21 21:18:19 -0400

    Add code for 12-navigating

--- lessons/01-setting-up/modules/Repos.js
+++ lessons/01-setting-up/modules/Repos.js
@@ -1,9 +1,23 @@
 // modules/Repos.js
 import React from 'react'
 import NavLink from './NavLink'
-import Repo from './Repo'
 
 export default React.createClass({
+
+  // ask for `router` from context
+  contextTypes: {
+    router: React.PropTypes.object
+  },
+
+  handleSubmit(event) {
+    event.preventDefault()
+    const userName = event.target.elements[0].value
+    const repo = event.target.elements[1].value
+    const path = `/repos/${userName}/${repo}`
+    console.log(path)
+    this.context.router.push(path)
+  },
+
   render() {
     return (
       <div>
@@ -12,7 +26,15 @@ export default React.createClass({
         <ul>
           <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
           <li><NavLink to="/repos/facebook/react">React</NavLink></li>
+          <li>
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" placeholder="userName"/> / {' '}
+              <input type="text" placeholder="repo"/>{' '}
+              <button type="submit">Go</button>
+            </form>
+          </li>
         </ul>
+
         {/* will render `Repo.js` when at /repos/:userName/:repoName */}
         {this.props.children}
       </div>
